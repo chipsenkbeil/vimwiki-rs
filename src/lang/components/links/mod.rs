03d24c99ae99817fa616b58a6f670d8184e7e892
Chip Senkbeil <chip@senkbeil.org>
2020-08-16 23:15:06 -0500

    Add note about discovery of additional link/transclusion formats

--- src/lang/components/links/mod.rs
+++ src/lang/components/links/mod.rs
@@ -19,8 +19,35 @@ pub use transclusion::TransclusionLink;
 mod wiki;
 pub use wiki::WikiLink;
 
+// TODO: Inspecting vimwiki source code, there are a couple of link utils
+//
+// 1. s:is_web_link = https | http | www | ftp | file | mailto
+// 2. s:is_img_link = .png | .jpg | .gif | .jpeg
+//
+// TRANSCLUSIONS
+// NOTE: Can include additional attributes beyond description
+//
+// {{imgurl|arg1|arg2}}         -> ???
+// {{imgurl}}                   -> <img src="imgurl"/>
+// {{imgurl|descr|style="A"}}   -> <img src="imgurl" alt="descr" style="A" />
+// {{imgurl|descr|class="B"}}   -> <img src="imgurl" alt="descr" class="B" />
+//
+// WIKILINKS
+// NOTE: According to below, only need to worry about transclusion other than
+//       string in a description
+//
+// [url]]                       -> <a href="url.html">url</a>
+// [[url|descr]]                -> <a href="url.html">descr</a>
+// [[url|{{...}}]]              -> <a href="url.html"> ... </a>
+// [[fileurl.ext|descr]]        -> <a href="fileurl.ext">descr</a>
+// [[dirurl/|descr]]            -> <a href="dirurl/index.html">descr</a>
+// [[url#a1#a2]]                -> <a href="url.html#a1-a2">url#a1#a2</a>
+// [[#a1#a2]]                   -> <a href="#a1-a2">#a1#a2</a>
+//
+
 /// Represents support for a description
 pub trait WithDescription {
+    // TODO: Support transclusion for embedding a thumbnail
     fn with_description(&mut self, description: String) -> &mut Self;
     fn description(&self) -> Option<&str>;
 }
