e6c970ce6ac66d50a8770aa6c930f0ddddc6b759
Chip Senkbeil <chip@senkbeil.org>
2020-09-06 11:44:56 -0500

    Add tests for wiki_link

--- src/lang/parsers/vimwiki/links/wiki.rs
+++ src/lang/parsers/vimwiki/links/wiki.rs
@@ -43,7 +43,10 @@ pub(super) fn wiki_link_internal(input: Span) -> VimwikiIResult<WikiLink> {
     let (input, maybe_anchor) = opt(preceded(
         tag("#"),
         map(
-            separated_list(tag("#"), take_line_while1(not(tag("#")))),
+            separated_list(
+                tag("#"),
+                take_line_while1(not(alt((tag("|"), tag("#"), tag("]]"))))),
+            ),
             |mut x| {
                 Anchor::new(
                     x.drain(..).map(|a| a.fragment().to_string()).collect(),
@@ -87,49 +90,187 @@ mod tests {
     use super::*;
 
     #[test]
+    fn wiki_link_should_fail_if_does_not_have_proper_prefix() {
+        let input = Span::new("link]]");
+        let result = wiki_link(input);
+
+        // No input should have been consumed
+        assert_eq!(*input.fragment(), "link]]");
+
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn wiki_link_should_fail_if_does_not_have_proper_suffix() {
+        let input = Span::new("[[link");
+        let result = wiki_link(input);
+
+        // No input should have been consumed
+        assert_eq!(*input.fragment(), "[[link");
+
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn wiki_link_should_not_consume_across_lines() {
+        let input = Span::new("[[link\n]]");
+        let result = wiki_link(input);
+
+        // No input should have been consumed
+        assert_eq!(*input.fragment(), "[[link\n]]");
+
+        assert!(result.is_err());
+    }
+
+    #[test]
     fn wiki_link_should_support_plain_link() {
-        // [[This is a link]]
-        todo!();
+        let input = Span::new("[[This is a link]]");
+        let (input, link) =
+            wiki_link(input).expect("Parser unexpectedly failed");
+
+        // Link should be consumed
+        assert!(input.fragment().is_empty());
+
+        assert_eq!(link.path.to_str().unwrap(), "This is a link");
+        assert_eq!(link.description, None);
+        assert_eq!(link.anchor, None);
     }
 
     #[test]
     fn wiki_link_should_support_description() {
-        // [[This is a link source|Description of the link]]
-        todo!();
+        let input =
+            Span::new("[[This is a link source|Description of the link]]");
+        let (input, link) =
+            wiki_link(input).expect("Parser unexpectedly failed");
+
+        // Link should be consumed
+        assert!(input.fragment().is_empty());
+
+        assert_eq!(link.path.to_str().unwrap(), "This is a link source");
+        assert_eq!(
+            link.description,
+            Some(Description::Text("Description of the link".to_string()))
+        );
+        assert_eq!(link.anchor, None);
     }
 
     #[test]
     fn wiki_link_should_support_sources_in_subdirectories() {
-        // [[projects/Important Project 1]]
-        todo!();
+        let input = Span::new("[[projects/Important Project 1]]");
+        let (input, link) =
+            wiki_link(input).expect("Parser unexpectedly failed");
+
+        // Link should be consumed
+        assert!(input.fragment().is_empty());
+
+        assert_eq!(link.path.to_str().unwrap(), "projects/Important Project 1");
+        assert_eq!(link.description, None);
+        assert_eq!(link.anchor, None);
     }
 
     #[test]
     fn wiki_link_should_support_relative_sources() {
-        // [[../index]]
-        todo!();
+        let input = Span::new("[[../index]]");
+        let (input, link) =
+            wiki_link(input).expect("Parser unexpectedly failed");
+
+        // Link should be consumed
+        assert!(input.fragment().is_empty());
+
+        assert_eq!(link.path.to_str().unwrap(), "../index");
+        assert_eq!(link.description, None);
+        assert_eq!(link.anchor, None);
     }
 
     #[test]
     fn wiki_link_should_support_absolute_source_for_wiki_root() {
-        // [[/index]]
-        todo!();
+        let input = Span::new("[[/index]]");
+        let (input, link) =
+            wiki_link(input).expect("Parser unexpectedly failed");
+
+        // Link should be consumed
+        assert!(input.fragment().is_empty());
+
+        assert_eq!(link.path.to_str().unwrap(), "/index");
+        assert_eq!(link.description, None);
+        assert_eq!(link.anchor, None);
     }
 
     #[test]
     fn wiki_link_should_support_source_being_subdirectory() {
-        // [[a subdirectory/|Other files]]
-        todo!();
+        let input = Span::new("[[a subdirectory/|Other files]]");
+        let (input, link) =
+            wiki_link(input).expect("Parser unexpectedly failed");
+
+        // Link should be consumed
+        assert!(input.fragment().is_empty());
+
+        assert_eq!(link.path.to_str().unwrap(), "a subdirectory/");
+        assert_eq!(
+            link.description,
+            Some(Description::Text("Other files".to_string()))
+        );
+        assert_eq!(link.anchor, None);
     }
+
     #[test]
-    fn wiki_link_should_support_anchors() {
-        // [[Todo List#Tomorrow|Tasks for tomorrow]]
-        todo!();
+    fn wiki_link_should_support_an_anchor() {
+        let input = Span::new("[[Todo List#Tomorrow|Tasks for tomorrow]]");
+        let (input, link) =
+            wiki_link(input).expect("Parser unexpectedly failed");
+
+        // Link should be consumed
+        assert!(input.fragment().is_empty());
+
+        assert_eq!(link.path.to_str().unwrap(), "Todo List");
+        assert_eq!(
+            link.description,
+            Some(Description::Text("Tasks for tomorrow".to_string()))
+        );
+        assert_eq!(
+            link.anchor,
+            Some(Anchor::new(vec!["Tomorrow".to_string()]))
+        );
+    }
+
+    #[test]
+    fn wiki_link_should_support_multiple_anchors() {
+        let input =
+            Span::new("[[Todo List#Tomorrow#Later|Tasks for tomorrow]]");
+        let (input, link) =
+            wiki_link(input).expect("Parser unexpectedly failed");
+
+        // Link should be consumed
+        assert!(input.fragment().is_empty());
+
+        assert_eq!(link.path.to_str().unwrap(), "Todo List");
+        assert_eq!(
+            link.description,
+            Some(Description::Text("Tasks for tomorrow".to_string()))
+        );
+        assert_eq!(
+            link.anchor,
+            Some(Anchor::new(vec![
+                "Tomorrow".to_string(),
+                "Later".to_string()
+            ]))
+        );
     }
 
     #[test]
     fn wiki_link_should_support_anchor_only() {
-        // [[#Tomorrow]]
-        todo!();
+        let input = Span::new("[[#Tomorrow]]");
+        let (input, link) =
+            wiki_link(input).expect("Parser unexpectedly failed");
+
+        // Link should be consumed
+        assert!(input.fragment().is_empty());
+
+        assert_eq!(link.path.to_str().unwrap(), "");
+        assert_eq!(link.description, None,);
+        assert_eq!(
+            link.anchor,
+            Some(Anchor::new(vec!["Tomorrow".to_string()]))
+        );
     }
 }
