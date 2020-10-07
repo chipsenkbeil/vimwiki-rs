80f90127908bbb6e5658fd5da12753224e0a8a0e
Chip Senkbeil <chip@senkbeil.org>
2020-10-06 22:21:16 -0500

    Replace pretty_env_logger with flexi_logger

--- vimwiki-server/src/main.rs
+++ vimwiki-server/src/main.rs
@@ -1,9 +1,13 @@
+use flexi_logger::{LevelFilter, LogSpecification, Logger};
 use vimwiki_server::program;
 
 #[tokio::main]
 async fn main() {
-    pretty_env_logger::formatted_builder()
-        // .filter_level(log::LevelFilter::Trace)
-        .init();
+    // TODO: We need to parse the program arguments in the binary (here) versus
+    //       the library as we need to get the verbosity level to use with
+    //       the filter level here -v == info, -vv == debug, -vvv == trace
+    Logger::with(LogSpecification::default(LevelFilter::Error).finalize())
+        .start()
+        .unwrap_or_else(|e| panic!("Logger initialization failed with {}", e));
     program::run().await.expect("Program failed unexpectedly");
 }
