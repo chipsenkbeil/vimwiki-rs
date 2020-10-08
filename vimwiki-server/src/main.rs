22def4a1e31acbe29d0b9f35857277b186cdb1ea
Chip Senkbeil <chip@senkbeil.org>
2020-10-07 22:00:42 -0500

    Remove old todo

--- vimwiki-server/src/main.rs
+++ vimwiki-server/src/main.rs
@@ -5,9 +5,6 @@ use vimwiki_server::{program, Config};
 async fn main() {
     let config = Config::load();
 
-    // TODO: We need to parse the program arguments in the binary (here) versus
-    //       the library as we need to get the verbosity level to use with
-    //       the filter level here -v == info, -vv == debug, -vvv == trace
     Logger::with(LogSpecification::default(config.log_level()).finalize())
         .start()
         .unwrap_or_else(|e| panic!("Logger initialization failed with {}", e));
