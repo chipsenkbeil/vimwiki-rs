3c1cd5e8b998a217c047180086d02f0df4826c47
Chip Senkbeil <chip@senkbeil.org>
2020-10-24 00:30:59 -0500

    Update stdin processing to expect form {"id": ..., "payload"} so can echo id back in response for callback usage

--- vimwiki-server/src/program/stdin.rs
+++ vimwiki-server/src/program/stdin.rs
@@ -1,5 +1,24 @@
 use super::{graphql, Config, Program};
 use log::{error, info};
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+struct Input {
+    /// Id of incoming payload to use when sending back a response
+    id: usize,
+
+    /// Payload as serialized
+    payload: String,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+struct Output {
+    /// Id of outgoing payload
+    id: usize,
+
+    /// Payload as serialized JSON string
+    payload: String,
+}
 
 /// Spawns a worker to process stdin and communicate back over stdout & stderr
 pub async fn run(program: Program, _config: Config) {
@@ -10,16 +29,20 @@ pub async fn run(program: Program, _config: Config) {
     //       blocking limitations within tokio's implementation causing
     //       problems: https://github.com/tokio-rs/tokio/issues/2466
     let stdin = std::io::stdin();
-    let mut input = String::new();
+    let mut buffer = String::new();
     loop {
-        let result = stdin.read_line(&mut input);
+        let result = stdin.read_line(&mut buffer);
         match result {
             Ok(n) if n > 0 => {
-                let result = schema.execute(&input).await;
-                match serde_json::to_string(&result) {
-                    Ok(json) => println!("{}", json),
-                    Err(x) => eprintln!("{}", x),
+                // Parse our input if possible, in the form of
+                // { "id": ..., "payload": ... }
+                if let Ok(Input { id, payload }) = serde_json::from_str(&buffer)
+                {
+                    let response = schema.execute(&payload).await;
+                    send_response(id, response).await;
                 }
+
+                buffer.clear();
             }
             Ok(_) => break,
             Err(x) => {
@@ -27,5 +50,15 @@ pub async fn run(program: Program, _config: Config) {
                 break;
             }
         }
+    }
+}
+
+async fn send_response(id: usize, response: async_graphql::Response) {
+    match serde_json::to_string(&response)
+        .map(|payload| Output { id, payload })
+        .and_then(|output| serde_json::to_string(&output))
+    {
+        Ok(msg) => println!("{}", msg),
+        Err(x) => eprintln!("{}", x),
     }
 }
