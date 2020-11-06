5a8dbbfa7ea58e1acdc642c4e84e57c5d20f1fa8
Chip Senkbeil <chip@senkbeil.org>
2020-11-05 22:50:55 -0600

    Fix compilation issues when using timekeeper feature

--- vimwiki/src/lang/parsers/utils/error.rs
+++ vimwiki/src/lang/parsers/utils/error.rs
@@ -3,6 +3,9 @@
 #[cfg(not(feature = "timekeeper"))]
 pub use nom::error::context;
 
+#[cfg(feature = "timekeeper")]
+use crate::lang::parsers::{IResult, Span};
+
 /// Wraps a parser in a contextual label, which makes it easier to identify
 /// where parsing failures occur. This implementation also logs to a
 /// timekeeper table, which can be printed out to evaluate the time spent
