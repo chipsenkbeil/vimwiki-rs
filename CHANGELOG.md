af88eb732a842522bb38091f1fa92cbd4c79aba0
Chip Senkbeil <chip@senkbeil.org>
2021-04-25 11:21:25 -0500

    Fix clippy warnings and update changelog with entity_macros change

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,9 @@
 - This `CHANGELOG.md` file to keep track of future changes
 - `scripts/release.sh` to keep track of all version changes and update multiple
   `Cargo.toml` as well as other files like this changelog
+- `vimwiki_<ELEMENT>_format` and `vimwiki_<ELEMENT>_raw_format` support into
+  the `vimwiki_macros` crate to support injecting content into vimwiki macros
+  at compile-time [#102](https://github.com/chipsenkbeil/vimwiki-rs/issues/102)
 
 ### Changed
 
--- vimwiki_macros/src/utils.rs
+++ vimwiki_macros/src/utils.rs
@@ -1,5 +1,5 @@
 use crate::error::{Error, Result};
-use proc_macro2::{token_stream::IntoIter as TokenIter, Span, TokenTree};
+use proc_macro2::{Span, TokenTree};
 
 /// Converts a token tree that is a string or byte string into a Rust string
 /// instance. Removes any blank lines (whitespace only) before and after
@@ -89,28 +89,4 @@ pub fn input_to_string(token: TokenTree, raw_mode: bool) -> Result<String> {
         };
 
     Ok(lines)
-}
-
-/// Validates that our macro receives only a single argument, advancing
-/// the input as a consequence
-pub fn require_empty_or_trailing_comma(input: &mut TokenIter) -> Result<()> {
-    let first = match input.next() {
-        Some(TokenTree::Punct(punct)) if punct.as_char() == ',' => {
-            match input.next() {
-                Some(second) => second,
-                None => return Ok(()),
-            }
-        }
-        Some(first) => first,
-        None => return Ok(()),
-    };
-    let last = input.last();
-
-    let begin_span = first.span();
-    let end_span = last.as_ref().map_or(begin_span, TokenTree::span);
-    let msg = format!(
-        "unexpected {token} in macro invocation; vimwiki argument must be a single string literal",
-        token = if last.is_some() { "tokens" } else { "token" }
-    );
-    Err(Error::new2(begin_span, end_span, &msg))
 }
