303758557ada62711b05cc8cdde22e9a7b8e6d09
Chip Senkbeil <chip@senkbeil.org>
2021-05-30 19:57:21 -0500

    More improvements to top-level cli error reporting

--- vimwiki-cli/src/main.rs
+++ vimwiki-cli/src/main.rs
@@ -5,26 +5,52 @@ mod subcommand;
 mod utils;
 
 use ast::Ast;
+use log::*;
 use opt::*;
 use structopt::StructOpt;
+
+enum ExitCodes {
+    FailedToLoadConfig = 1,
+    FailedToLoadData = 2,
+    SubcommandFailed = 3,
+}
+
+impl ExitCodes {
+    pub fn exit(self) -> ! {
+        std::process::exit(self as i32);
+    }
+}
 
 fn main() {
     let opt = Opt::from_args();
     init_logging(&opt.common);
 
-    let config =
-        utils::load_html_config(&opt.common, opt.subcommand.extra_paths())
-            .expect("Failed to load config");
-    let ast = Ast::load(
+    let config = match utils::load_html_config(
+        &opt.common,
+        opt.subcommand.extra_paths(),
+    ) {
+        Ok(config) => config,
+        Err(x) => {
+            error!("Failed to load config: {}", x);
+            ExitCodes::FailedToLoadConfig.exit();
+        }
+    };
+
+    let ast = match Ast::load(
         &config,
         &opt.common.include,
         &opt.common.cache,
         opt.common.no_cache,
         opt.common.no_prune_cache,
-    )
-    .expect("Failed to load data");
+    ) {
+        Ok(ast) => ast,
+        Err(x) => {
+            error!("Failed to load data: {}", x);
+            ExitCodes::FailedToLoadData.exit();
+        }
+    };
 
-    match opt.subcommand {
+    let res = match opt.subcommand {
         Subcommand::Convert(cmd) => {
             subcommand::convert(cmd, opt.common, config, ast)
         }
@@ -34,8 +60,12 @@ fn main() {
         Subcommand::Inspect(cmd) => {
             subcommand::inspect(cmd, opt.common, config, ast)
         }
+    };
+
+    if let Err(x) = res {
+        error!("Subcommand failed: {}", x);
+        ExitCodes::SubcommandFailed.exit();
     }
-    .expect("Command failed unexpectedly");
 }
 
 fn init_logging(opt: &CommonOpt) {
